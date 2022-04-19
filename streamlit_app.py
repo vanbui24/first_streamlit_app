1a5aa33c05b7e785ba74e8e6f1448f13c79a4838
vabui1 <vanbui24@gmail.com>
2022-04-19 13:52:43 +0200

    added header & text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title('My Mom\'s New Healthy Dinrer')
+
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
