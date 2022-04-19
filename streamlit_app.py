7e6a7cf3b8c763a4ecf60dd6a6fc696b50860d9b
vabui1 <vanbui24@gmail.com>
2022-04-19 14:26:27 +0200

    added fruit as index

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,9 +10,10 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🍊 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index("Fruit")
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
