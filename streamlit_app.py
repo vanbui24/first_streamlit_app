f698f10b084ad9582e918ddee5b2e3c4623ffe4d
vabui1 <vanbui24@gmail.com>
2022-04-21 23:27:46 +0200

    added correct fruit_name to sf

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,6 @@ def get_fruityvice_data(this_fruit_choice):
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 
-
 streamlit.header('Fruityvice Fruit Advice!')
 try:
     fruit_choice = streamlit.text_input("What fruit would you like information about?")
@@ -59,11 +58,9 @@ if streamlit.button("Get Fruit Load List"):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
-
-
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        my_cur.execute("insert into fruit_load_list values ('" + new_fruit +"')")
         return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input("What fruit would you like add?")
